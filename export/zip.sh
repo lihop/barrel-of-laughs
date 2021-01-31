a139895653a1d5f553819ec8ad684937498e9483
Leroy Hopson <git@leroy.geek.nz>
2021-01-31 09:36:44 +0700

    Update zip script

--- export/zip.sh
+++ export/zip.sh
@@ -1,3 +1,4 @@
+rm ./*.zip
 zip -r barrel_of_laughs.x11.zip barrel_of_laughs.x11/*
 zip -r barrel_of_laughs.osx.zip barrel_of_laughs.osx/*
 zip -r barrel_of_laughs.windows.zip barrel_of_laughs.windows/*
